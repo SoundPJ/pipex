249856d6ec69d99d9311a80453809cc3900f0781
Penfah Jerddeesakul <pjerddee@z3t3c4.42bangkok.com>
2022-08-11 04:50:55 +0700

    Backup

--- ft_pipex.h
+++ ft_pipex.h
@@ -6,17 +6,19 @@
 /*   By: pjerddee <pjerddee@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/24 14:40:49 by pjerddee          #+#    #+#             */
-/*   Updated: 2022/08/09 00:36:45 by pjerddee         ###   ########.fr       */
+/*   Updated: 2022/08/10 20:38:31 by pjerddee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PIPEX_H
 # define FT_PIPEX_H
+# define BUFFER_SIZE 2
 
 # include <stdio.h>
 # include <unistd.h>
 # include <sys/wait.h>
 # include <stdlib.h>
+# include <fcntl.h>
 # include "libft.h"
 
 #endif
