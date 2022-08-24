f4676ea9224cfe8f424328c1ebad87606dd68ff0
SoundPJ <57010936@kmitl.ac.th>
2022-08-24 13:33:45 +0700

    backup

--- pipex.c
+++ pipex.c
@@ -6,13 +6,13 @@
 /*   By: pjerddee <pjerddee@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/14 20:01:28 by pjerddee          #+#    #+#             */
-/*   Updated: 2022/08/23 23:09:18 by pjerddee         ###   ########.fr       */
+/*   Updated: 2022/08/24 13:32:36 by pjerddee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-void	here_doc(char **av, int *hd_fd);
+char	*here_doc(char **av);
 
 int	main(int argc, char **argv, char **env)
 {
@@ -28,12 +28,13 @@ int	main(int argc, char **argv, char **env)
 		if (ft_strncmp(argv[1], "here_doc", 8) == 0)
 		{
 			pipe(hd_fd);
-			here_doc(argv, hd_fd);
-			printf("DONE HD\n");
+			ft_putstr_fd(here_doc(argv), 1);
+			printf("DONE HERE_DOC\n");
+			ft_putstr_fd(here_doc(argv), hd_fd[1]);
 			dup2(hd_fd[0], 0);
-			ft_pipex(argc, argv, env, 3);
 			close(hd_fd[0]);
 			close(hd_fd[1]);
+			ft_pipex(argc, argv, env, 3);
 		}
 		else
 		{
@@ -44,23 +45,23 @@ int	main(int argc, char **argv, char **env)
 	return (0);
 }
 
-void	here_doc(char **av, int *hd_fd)
+char	*here_doc(char **av)
 {
 	char	*limiter;
 	char	*line;
 	char	*input_s;
 
 	limiter = ft_strjoin(av[2], "\n");
+	write(1, ">", 1);
 	line = get_next_line(0);
 	input_s = ft_calloc(ft_strlen(line) + 1, sizeof(char));
-	ft_strlcpy(input_s, line, ft_strlen(line));
-	input_s = ft_strjoin(input_s, "\n");
+	// ft_strlcpy(input_s, line, ft_strlen(line));
+	// input_s = ft_strjoin(line, "\n");
 	while (ft_strncmp(line, limiter, ft_strlen(av[2])))
 	{
-		printf("line: %s\t av[2]: %s\n", line, av[2]);
-		line = get_next_line(0);
+		write(1, ">", 1);
 		input_s = ft_strjoin(input_s, line);
+		line = get_next_line(0);
 	}
-	// printf("%s", input_s);
-	ft_putstr_fd(input_s, hd_fd[1]);
+	return (input_s);
 }
